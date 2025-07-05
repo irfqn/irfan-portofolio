2e29fd07ddc1043e37e6aa8073d578e83c9f6e35
irfqn <irfanalrasyid96@gmail.com>
2025-07-05 17:44:38 +0700

    Done with ABOUT component responsivity

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import irfan from "../assets/irfan.png"
 const DescAbout = () =>{
     return(
         <div>
-            <p className="text-white my-8 pr-20 text-justify">A fresh graduate in Computer Engineering with a strong interest in web development. Experienced in frontend and backend development, with projects involving cloud technology deployment. Actively improving skills through independent projects and training.</p>
+            <p className="text-white my-8 pr-20 text-justify max-md:pr-0">A fresh graduate in Computer Engineering with a strong interest in web development. Experienced in frontend and backend development, with projects involving cloud technology deployment. Actively improving skills through independent projects and training.</p>
             <div className="flex gap-32">
                 <div>
                     <p className="text-white opacity-50 text-xs">Graduated from</p>
@@ -28,7 +28,7 @@ const About = () =>{
     }
 
     return(
-        <div className="flex max-md:flex-col gap-4">
+        <div className="flex max-md:flex-col gap-20">
             <div className="kiri w-full h-auto">
                 <button 
                 className="px-7 py-1 text-white border rounded-[20px]"
@@ -39,10 +39,10 @@ const About = () =>{
             <div className="kanan w-full h-auto">
                 <p className="text-white text-2xl ">I’m Irfan, a Web Developer crafting innovative solutions and bringing ideas to life through code and cloud technology.</p>
                 <div className="flex my-16">
-                    <img src={irfan} alt="foto_irfan" className="w-1/4"/>
-                    <div className="px-5 flex flex-col justify-between">
-                        <p className="font-semibold text-white">Future-Ready Development</p>
-                        <p className="text-lg font-normal leading-6 text-gray-300">Developing solutions that scale with tomorrows technology.</p>
+                    <img src={irfan} alt="foto_irfan" className="w-1/4 max-sm:w-[45%]"/>
+                    <div className="px-5 flex flex-col justify-between max-sm:pl-5 max-sm:px-0">
+                        <p className="font-semibold text-white max-sm:text-sm">Future-Ready Development</p>
+                        <p className="text-lg font-normal leading-6 text-gray-300 max-sm:text-sm">Developing solutions that scale with tomorrows technology.</p>
                     </div>
                 </div>
             </div>
